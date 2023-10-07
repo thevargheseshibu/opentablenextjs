4662c36ebd8f450990171fd7c54e9c953fbb6335
Varghese Shibu <43093582+thevargheseshibu@users.noreply.github.com>
2023-10-07 17:42:11 +0530

    Change: Resuable Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-
 import { Inter } from '@next/font/google'
-import Link from "next/link"
 import NavBar from './components/NavBar';
+import Header from './components/Header';
+import RestaurantCard from './components/RestaurantCard';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,42 +12,10 @@ export default function Home() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
-        {/* NAVBAR */}
         <NavBar />
-        {/* NAVBAR */}
         <main>
-          {/* HEADER */}
-
-          {/* HEADER */} {/* CARDS */}
-          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            {/* CARD */}
-            <Link href="/restaurant/milestones-grill">
-              <div
-                className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer"
-              >
-                <img
-                  src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
-                  alt=""
-                  className="w-full h-36"
-                />
-                <div className="p-1">
-                  <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
-                  <div className="flex items-start">
-                    <div className="flex mb-2">*****</div>
-                    <p className="ml-2">77 reviews</p>
-                  </div>
-                  <div className="flex text-reg font-light capitalize">
-                    <p className=" mr-3">Mexican</p>
-                    <p className="mr-3">$$$$</p>
-                    <p>Toronto</p>
-                  </div>
-                  <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
-                </div>
-              </div>
-            </Link>
-            {/* CARD */}
-          </div>
-          {/* CARDS */}
+          <Header />
+          <RestaurantCard />
         </main>
       </main>
     </main>
