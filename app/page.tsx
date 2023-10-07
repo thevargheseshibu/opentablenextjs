683261c6ef801fcd339ad36f0361209ed98866d5
Varghese Shibu <43093582+thevargheseshibu@users.noreply.github.com>
2023-10-07 17:23:18 +0530

    Feat : Programmatic Navigation using useRouter

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import Link from "next/link"
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router =useRouter()
+  const  [location,setLocation] = useState("")
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
     <main className="max-w-screen-2xl m-auto bg-white">
@@ -37,8 +43,16 @@ export default function Home() {
                 className="rounded  mr-3 p-2 w-[450px]"
                 type="text"
                 placeholder="State, city or town"
+                value={location}
+                onChange={(e)=>setLocation(e.target.value)}
               />
-              <button className="rounded bg-red-600 px-9 py-2 text-white">
+              <button className="rounded bg-red-600 px-9 py-2 text-white"
+              onClick={()=>{if(location==="banana"){
+                return
+              }
+              else{
+                router.push("./search")
+              }}}>
                 Let's go
               </button>
             </div>
