e5c87b71f88acea1d149cf672ba75b940c5862df
Varghese Shibu <43093582+thevargheseshibu@users.noreply.github.com>
2023-10-07 17:36:58 +0530

    Change: Header Mod

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import Image from 'next/image'
+
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
 import Link from "next/link"
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
 import NavBar from './components/NavBar';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const router = useRouter()
-  const [location, setLocation] = useState("")
+
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
@@ -21,35 +17,7 @@ export default function Home() {
         {/* NAVBAR */}
         <main>
           {/* HEADER */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-            <div className="text-center mt-10">
-              <h1 className="text-white text-5xl font-bold mb-2">
-                Find your table for any occasion
-              </h1>
-              {/* SEARCH BAR */}
-              <div className="text-left text-lg py-3 m-auto flex justify-center">
-                <input
-                  className="rounded  mr-3 p-2 w-[450px]"
-                  type="text"
-                  placeholder="State, city or town"
-                  value={location}
-                  onChange={(e) => setLocation(e.target.value)}
-                />
-                <button className="rounded bg-red-600 px-9 py-2 text-white"
-                  onClick={() => {
-                    if (location === "banana") {
-                      return
-                    }
-                    else {
-                      router.push("./search")
-                    }
-                  }}>
-                  Let's go
-                </button>
-              </div>
-              {/* SEARCH BAR */}
-            </div>
-          </div>
+
           {/* HEADER */} {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
             {/* CARD */}
