fff2445e196777bb4354373662bf1bee4cef869e
Varghese Shibu <43093582+thevargheseshibu@users.noreply.github.com>
2023-10-07 18:36:23 +0530

    Change: Compartmentalising Reserve.jsx

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import Header from "./components/Header"
+import Form from "./components/Form"
 
 export default function Reserve() {
     return (
@@ -23,70 +25,8 @@ export default function Reserve() {
                 {/* NAVBAR END */}
                 <div className="border-t h-screen">
                     <div className="py-9 w-3/5 m-auto">
-                        {/* HEADER */}
-                        <div>
-                            <h3 className="font-bold">You're almost done!</h3>
-                            <div className="mt-5 flex">
-                                <img
-                                    src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-                                    alt=""
-                                    className="w-32 h-18 rounded"
-                                />
-                                <div className="ml-4">
-                                    <h1 className="text-3xl font-bold">
-                                        Aiāna Restaurant Collective
-                                    </h1>
-                                    <div className="flex mt-3">
-                                        <p className="mr-6">Tues, 22, 2023</p>
-                                        <p className="mr-6">7:30 PM</p>
-                                        <p className="mr-6">3 people</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* HEADER */} {/* FORM */}
-                        <div className="mt-10 flex flex-wrap justify-between w-[660px]">
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="First name"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Last name"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Phone number"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Email"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Occasion (optional)"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Requests (optional)"
-                            />
-                            <button
-                                className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300"
-                            >
-                                Complete reservation
-                            </button>
-                            <p className="mt-4 text-sm">
-                                By clicking “Complete reservation” you agree to the OpenTable Terms
-                                of Use and Privacy Policy. Standard text message rates may apply.
-                                You may opt out of receiving text messages at any time.
-                            </p>
-                        </div>
+                        <Header />
+                        < Form />
                     </div>
                 </div>
             </main>
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link"
 import NavBar from "../../../components/NavBar"
 import Header from "../components/Header"
 import RestaurantNavbar from "../components/RestaurantNavbar"
