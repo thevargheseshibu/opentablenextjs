f7a19850015296ffd055d827a8c02c94fc54160e
Varghese Shibu <43093582+thevargheseshibu@users.noreply.github.com>
2023-10-08 11:40:23 +0530

    Chores: Initializing Type

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,44 @@
-"use client";
-
-import { Inter } from '@next/font/google'
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
+import { PrismaClient ,Cuisine,Location,PRICE} from '@prisma/client';
 
-const inter = Inter({ subsets: ['latin'] })
+export interface RestaurantCardType {
+  id: number,
+  name: string;
+  main_image:string;
+  cuisine:Cuisine
+  location:Location,
+  price : PRICE,
+  slug:string
+  
+}
+
+const prisma = new PrismaClient()
+
+const fetchRestaurants = async () : Promise<RestaurantCardType[]> =>{
+  const restaurants = await prisma.restaurant.findMany({
+    select:{
+      id:true,
+      name:true,
+      main_image:true,
+      cuisine:true,
+      location:true,
+      price:true,
+      slug:true
+    }
+  });
+  return restaurants
+}
  
-export default function Home() {
-
+export default async function Home() {
+const restaurants = await fetchRestaurants();
+console.log("restaurants",restaurants)
   return (
     <main>
       <Header />
       <div></div>
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestaurantCard />
+      {restaurants.map((restaurant)=><RestaurantCard restaurant={restaurant} />)}
       </div>
     </main>
   )
