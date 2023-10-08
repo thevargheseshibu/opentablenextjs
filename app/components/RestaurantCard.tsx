5f287a3de315261885f350174de55cfdc887f0a7
Varghese Shibu <43093582+thevargheseshibu@users.noreply.github.com>
2023-10-08 17:23:16 +0530

    Chores : Changes

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -1,39 +1,34 @@
-"use client"
-import Link from 'next/link'
-import { RestaurantCardType } from '../page'
-import Price from './Price'
+import Link from "next/link";
+import { RestaurantCardType } from "../page";
+import Price from "./Price";
+import Stars from "./Stars";
 
 interface Props {
-  restaurant:RestaurantCardType
+  restaurant: RestaurantCardType;
 }
 
-export default function RestaurantCard({restaurant}:Props) {
+export default function RestaurantCard({ restaurant }: Props) {
   return (
-    
-    <Link href={`/restaurant/${restaurant.slug}`}>
-      <div
-        className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer"
-      >
-        <img
-          src={restaurant.main_image}
-          alt=""
-          className="w-full h-36"
-        />
+    <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
+      <Link href={`/restaurant/${restaurant.slug}`}>
+        <img src={restaurant.main_image} alt="" className="w-full h-36" />
         <div className="p-1">
           <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
-            <div className="flex mb-2">*****</div>
-            <p className="ml-2">77 reviews</p>
+            <Stars reviews={restaurant.reviews} />
+            <p className="ml-2">
+              {restaurant.reviews.length} review
+              {restaurant.reviews.length === 1 ? "" : "s"}
+            </p>
           </div>
           <div className="flex text-reg font-light capitalize">
             <p className=" mr-3">{restaurant.cuisine.name}</p>
             <Price price={restaurant.price} />
-            <p>Toronto</p>
+            <p>{restaurant.location.name}</p>
           </div>
           <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
         </div>
-      </div>
-    </Link>
-  )
+      </Link>
+    </div>
+  );
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,48 @@
-import Header from './components/Header';
-import RestaurantCard from './components/RestaurantCard';
-import { PrismaClient ,Cuisine,Location,PRICE} from '@prisma/client';
+import Header from "./components/Header";
+import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient, Cuisine, Location, PRICE, Review } from "@prisma/client";
 
 export interface RestaurantCardType {
-  id: number,
+  id: number;
   name: string;
-  main_image:string;
-  cuisine:Cuisine
-  location:Location,
-  price : PRICE,
-  slug:string
-  
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
+  reviews: Review[];
 }
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const fetchRestaurants = async () : Promise<RestaurantCardType[]> =>{
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
   const restaurants = await prisma.restaurant.findMany({
-    select:{
-      id:true,
-      name:true,
-      main_image:true,
-      cuisine:true,
-      location:true,
-      price:true,
-      slug:true
-    }
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      slug: true,
+      location: true,
+      price: true,
+      reviews: true,
+    },
   });
-  return restaurants
-}
- 
+
+  return restaurants;
+};
+
 export default async function Home() {
-const restaurants = await fetchRestaurants();
-console.log("restaurants",restaurants)
+  const restaurants = await fetchRestaurants();
+
   return (
     <main>
       <Header />
-      <div></div>
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-      {restaurants.map((restaurant)=><RestaurantCard restaurant={restaurant} />)}
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
       </div>
     </main>
-  )
+  );
 }
- 
