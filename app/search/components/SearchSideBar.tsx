3ccebcbf2f31edef17aa691589618195023b4ed4
Varghese Shibu <43093582+thevargheseshibu@users.noreply.github.com>
2023-10-08 16:24:43 +0530

    Feats: Search completed

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -1,46 +1,90 @@
+import { Cuisine, Location, PRICE } from "@prisma/client";
+import Link from "next/link";
 
-function SearchSideBar() {
-    return (
+export default function SearchSideBar({
+  locations,
+  cuisines,
+  searchParams,
+}: {
+  locations: Location[];
+  cuisines: Cuisine[];
+  searchParams: { city?: string; cuisine?: string; price?: PRICE };
+}) {
+  const prices = [
+    {
+      price: PRICE.CHEAP,
+      label: "$",
+      className: "border w-full text-reg text-center font-light rounded-l p-2",
+    },
+    {
+      price: PRICE.REGULAR,
+      label: "$$",
+      className: "border w-full text-reg text-center font-light p-2",
+    },
+    {
+      price: PRICE.EXPENSIVE,
+      label: "$$$",
+      className: "border w-full text-reg text-center font-light rounded-r p-2",
+    },
+  ];
 
-        <div className="w-1/5">
-            <div className="border-b pb-4">
-                <h1 className="mb-2">Region</h1>
-                <p className="font-light text-reg">Toronto</p>
-                <p className="font-light text-reg">Ottawa</p>
-                <p className="font-light text-reg">Montreal</p>
-                <p className="font-light text-reg">Hamilton</p>
-                <p className="font-light text-reg">Kingston</p>
-                <p className="font-light text-reg">Niagara</p>
-            </div>
-            <div className="border-b pb-4 mt-3">
-                <h1 className="mb-2">Cuisine</h1>
-                <p className="font-light text-reg">Mexican</p>
-                <p className="font-light text-reg">Italian</p>
-                <p className="font-light text-reg">Chinese</p>
-            </div>
-            <div className="mt-3 pb-4">
-                <h1 className="mb-2">Price</h1>
-                <div className="flex">
-                    <button className="border w-full text-reg font-light rounded-l p-2">
-                        $
-                    </button>
-                    <button
-                        className="border-r border-t border-b w-full text-reg font-light p-2"
-                    >
-                        $$
-                    </button>
-                    <button
-                        className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r"
-                    >
-                        $$$
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="w-1/5">
+      <div className="border-b pb-4 flex flex-col">
+        <h1 className="mb-2">Region</h1>
+        {locations.map((location) => (
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                city: location.name,
+              },
+            }}
+            className="font-light text-reg capitalize"
+            key={location.id}
+          >
+            {location.name}
+          </Link>
+        ))}
+      </div>
+      <div className="border-b pb-4 mt-3 flex flex-col">
+        <h1 className="mb-2">Cuisine</h1>
+        {cuisines.map((cuisine) => (
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                cuisine: cuisine.name,
+              },
+            }}
+            className="font-light text-reg capitalize"
+            key={cuisine.id}
+          >
+            {cuisine.name}
+          </Link>
+        ))}
+      </div>
+      <div className="mt-3 pb-4">
+        <h1 className="mb-2">Price</h1>
+        <div className="flex">
+          {prices.map(({ price, label, className }) => (
+            <Link
+              href={{
+                pathname: "/search",
+                query: {
+                  ...searchParams,
+                  price,
+                },
+              }}
+              className={className}
+            >
+              {label}
+            </Link>
+          ))}
         </div>
-
-
-
-    )
+      </div>
+    </div>
+  );
 }
-
-export default SearchSideBar
