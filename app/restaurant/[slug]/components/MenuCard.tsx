6871904b1189d3b0b57cc7978046d81ce6d62045
Varghese Shibu <43093582+thevargheseshibu@users.noreply.github.com>
2023-10-08 14:51:56 +0530

    Chores: Menu card updated

--- app/restaurant/[slug]/components/MenuCard.tsx
+++ app/restaurant/[slug]/components/MenuCard.tsx
@@ -1,13 +1,11 @@
+import { Item } from "@prisma/client";
 
-
-export default function MenuCard() {
-    return (
-        <div className=" border rounded p-3 w-[49%] mb-3">
-            <h3 className="font-bold text-lg">Surf and Turf</h3>
-            <p className="font-light mt-1 text-sm">
-                A well done steak with lobster and rice
-            </p>
-            <p className="mt-7">$80.00</p>
-        </div>
-    )
+export default function MenuCard({ item }: { item: Item }) {
+  return (
+    <div className=" border rounded p-3 w-[49%] mb-3">
+      <h3 className="font-bold text-lg">{item.name}</h3>
+      <p className="font-light mt-1 text-sm">{item.description}</p>
+      <p className="mt-7">{item.price}</p>
+    </div>
+  );
 }
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,13 +1,40 @@
-import Header from "../components/Header"
-import RestaurantNavbar from "../components/RestaurantNavbar"
-import Menu from "../components/Menu"
-export default function RestaurantMenu() {
-    return (
-        <>
-                <div className="bg-white w-[100%] rounded p-3 shadow">
-                    <RestaurantNavbar />
-                    <Menu />
-                </div>
-        </>
-    )
+import { PrismaClient } from "@prisma/client";
+import Menu from "../components/Menu";
+import RestaurantNavbar from "../components/RestaurantNavbar";
+
+
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      items: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant.items;
+};
+
+export default async function RestaurantMenu({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const menu = await fetchRestaurantMenu(params.slug);
+
+  return (
+    <>
+      <div className="bg-white w-[100%] rounded p-3 shadow">
+        <RestaurantNavbar slug={params.slug} />
+        <Menu menu={menu} />
+      </div>
+    </>
+  );
 }
